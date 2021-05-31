7bc39e7b8d22be93392c39398362e36c9bea8a66
AboodXD <aboood40091@gmail.com>
2021-05-31 04:56:41 +0400

    math: Update matrix types to union of several types

--- include/math/math_MTX34.h
+++ include/math/math_MTX34.h
@@ -1,13 +1,18 @@
 #ifndef MATH_MTX34_H_
 #define MATH_MTX34_H_
 
-#include <types.h>
+#include <math/math_VEC4.h>
 
 namespace nw { namespace math {
 
 struct MTX34
 {
-    f32 rows[3][4];
+    union
+    {
+        f32  m[3][4];
+        VEC4 v[3];
+        f32  a[3 * 4];
+    };
 };
 
 } } // namespace nw::math
--- include/math/math_MTX44.h
+++ include/math/math_MTX44.h
@@ -1,13 +1,18 @@
 #ifndef MATH_MTX44_H_
 #define MATH_MTX44_H_
 
-#include <types.h>
+#include <math/math_VEC4.h>
 
 namespace nw { namespace math {
 
 struct MTX44
 {
-    f32 rows[4][4];
+    union
+    {
+        f32  m[4][4];
+        VEC4 v[4];
+        f32  a[4 * 4];
+    };
 };
 
 } } // namespace nw::math
