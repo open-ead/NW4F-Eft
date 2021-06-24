fcf35664e4c39c373fb57150eb8fef6f65fd9373
AboodXD <aboood40091@gmail.com>
2021-06-24 08:51:01 +0400

    EmitterCalc: Implement CalcEmitLineSameDivide()

--- src/eft_EmitterVolume.cpp
+++ src/eft_EmitterVolume.cpp
@@ -512,7 +512,7 @@ PtclInstance* EmitterCalc::CalcEmitLine(EmitterInstance* emitter)
     f32 velocityMag = emitter->anim[15] * emitter->emitterSet->_244;
 
     f32 scaleZ = data->volumeScale.z * emitterSet->_228.z * emitter->anim[22];
-    f32 length = data->_378 * scaleZ;
+    f32 center = data->_378 * scaleZ;
 
     PtclInstance* ptclFirst = NULL;
     for (s32 i = 0; i < numEmit; i++)
@@ -528,11 +528,94 @@ PtclInstance* EmitterCalc::CalcEmitLine(EmitterInstance* emitter)
 
         ptcl->pos.x = 0.0f;
         ptcl->pos.y = 0.0f;
-        ptcl->pos.z = emitter->random.GetF32() * scaleZ - (scaleZ + length) / 2.0f;
+        ptcl->pos.z = emitter->random.GetF32() * scaleZ - (scaleZ + center) / 2.0f;
 
         ptcl->velocity.x = 0.0f;
         ptcl->velocity.y = 0.0f;
         ptcl->velocity.z = velocityMag;
+
+        if (data->_408 != 0.0f)
+        {
+            math::VEC3 posXZ = (math::VEC3){ ptcl->pos.x, 0.0f, ptcl->pos.z };
+            if (posXZ.MagnitudeSquare() <= FLT_MIN) // FLT_MIN = 1.1754943508222875E-38f
+            {
+                posXZ.x = emitter->random.GetF32Range(-1.0f, 1.0f);
+                posXZ.z = emitter->random.GetF32Range(-1.0f, 1.0f);
+            }
+
+            if (posXZ.MagnitudeSquare() == 0.0f)
+                posXZ = (math::VEC3){ 0.0f, 0.0f, 0.0f };
+            else
+                posXZ.Normalize();
+
+            math::VEC3::Scale(&posXZ, &posXZ, data->_408);
+            math::VEC3::Add(&ptcl->velocity, &ptcl->velocity, &posXZ);
+        }
+
+        EmitCommon(emitter, ptcl);
+    }
+
+    emitter->isEmitted = true;
+    return ptclFirst;
+}
+
+PtclInstance* EmitterCalc::CalcEmitLineSameDivide(EmitterInstance* emitter)
+{
+    const SimpleEmitterData* data = emitter->data;
+    const EmitterSet* emitterSet = emitter->emitterSet;
+
+    f32 emissionRate = emitter->anim[0] * emitter->controller->_0;
+    emitter->emitLostRate += emissionRate;
+    s32 counter = (s32)emitter->counter;
+    if (counter == 0 && emitter->emitLostRate < 1.0f && emissionRate != 0.0f)
+        emitter->emitLostRate = 1.0f;
+
+    s32 numEmit = (s32)floorf(emitter->emitLostRate);
+    if (data->_289 != 0) numEmit = 1;
+    emitter->emitLostRate -= (f32)numEmit;
+    if (numEmit == 0)
+        return NULL;
+
+    f32 velocityMag = emitter->anim[15] * emitter->emitterSet->_244;
+
+    f32 scaleZ = data->volumeScale.z * emitterSet->_228.z * emitter->anim[22];
+    f32 center = data->_378 * scaleZ;
+
+    f32 pos;
+    f32 lengthUnit;
+
+    if (numEmit == 1)
+    {
+        pos = 0.5f;
+        lengthUnit = 0.0f;
+    }
+    else
+    {
+        pos = 0.0f;
+        lengthUnit = 1.0f / (f32)(numEmit - 1);
+    }
+
+    PtclInstance* ptclFirst = NULL;
+    for (s32 i = 0; i < numEmit; i++)
+    {
+        PtclInstance* ptcl = mSys->AllocPtcl(emitter->calc->GetPtclType());
+        if (ptclFirst == NULL)
+            ptclFirst = ptcl;
+        if (ptcl == NULL)
+            break;
+
+        ptcl->data = data;
+        ptcl->stripe = NULL;
+
+        ptcl->pos.x = 0.0f;
+        ptcl->pos.y = 0.0f;
+        ptcl->pos.z = pos * scaleZ - (scaleZ + center) / 2.0f;
+
+        ptcl->velocity.x = 0.0f;
+        ptcl->velocity.y = 0.0f;
+        ptcl->velocity.z = velocityMag;
+
+        pos += lengthUnit;
 
         if (data->_408 != 0.0f)
         {
