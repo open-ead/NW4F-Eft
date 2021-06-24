56603878ceb8cc0b94f4c06dcfc1d988baf162a7
AboodXD <aboood40091@gmail.com>
2021-06-24 08:20:01 +0400

    EmitterCalc: Implement CalcEmitCircleSameDivide()

--- src/eft_EmitterVolume.cpp
+++ src/eft_EmitterVolume.cpp
@@ -157,4 +157,94 @@ PtclInstance* EmitterCalc::CalcEmitCircle(EmitterInstance* emitter)
     return ptclFirst;
 }
 
+PtclInstance* EmitterCalc::CalcEmitCircleSameDivide(EmitterInstance* emitter)
+{
+    const SimpleEmitterData* data = emitter->data;
+    const EmitterSet* emitterSet = emitter->emitterSet;
+
+    f32 emissionRate = emitter->anim[0] * emitter->controller->_0;
+    emitter->emitLostRate += emissionRate;
+    s32 counter = (s32)emitter->counter;
+    if (counter == 0 && emitter->emitLostRate < 1.0f && emissionRate != 0.0f)
+        emitter->emitLostRate = 1.0f;
+
+    s32 numEmit = (s32)floorf(emitter->emitLostRate);
+    if (data->_289 != 0) numEmit = 1;
+    emitter->emitLostRate -= (f32)numEmit;
+    if (numEmit == 0)
+        return NULL;
+
+    f32 velocityMag = emitter->anim[15] * emitter->emitterSet->_244;
+
+    f32 scaleX = data->volumeScale.x * emitterSet->_228.x * emitter->anim[22];
+    f32 scaleZ = data->volumeScale.z * emitterSet->_228.z * emitter->anim[23];
+
+    u32 arcLengthUnit;
+    u32 arcStartAngle = data->arcStartAngle;
+    if (data->_287 != 0)
+        arcStartAngle = emitter->random.GetU32();
+
+    if (numEmit <= 1.0f) // Comparison with 1.0f instead of 1... mistake?
+        arcLengthUnit = 0;
+
+    else
+    {
+        if (data->arcLength == 0xFFFFFFFF)
+            arcLengthUnit = 0xFFFFFFFF / numEmit;
+        else
+            arcLengthUnit = data->arcLength / (numEmit - 1);
+    }
+
+    u32 angle = arcStartAngle;
+
+    PtclInstance* ptclFirst = NULL;
+    for (s32 i = 0; i < numEmit; i++)
+    {
+        PtclInstance* ptcl = mSys->AllocPtcl(emitter->calc->GetPtclType());
+        if (ptclFirst == NULL)
+            ptclFirst = ptcl;
+        if (ptcl == NULL)
+            break;
+
+        ptcl->data = data;
+        ptcl->stripe = NULL;
+
+        f32 sin_val, cos_val;
+        math::SinCosIdx(&sin_val, &cos_val, angle);
+
+        ptcl->pos.x = sin_val * scaleX;
+        ptcl->pos.y = 0.0f;
+        ptcl->pos.z = cos_val * scaleZ;
+
+        ptcl->velocity.x = sin_val * velocityMag;
+        ptcl->velocity.y = 0.0f;
+        ptcl->velocity.z = cos_val * velocityMag;
+
+        angle += arcLengthUnit;
+
+        if (data->_408 != 0.0f)
+        {
+            math::VEC3 posXZ = (math::VEC3){ ptcl->pos.x, 0.0f, ptcl->pos.z };
+            if (posXZ.MagnitudeSquare() <= FLT_MIN) // FLT_MIN = 1.1754943508222875E-38f
+            {
+                posXZ.x = emitter->random.GetF32Range(-1.0f, 1.0f);
+                posXZ.z = emitter->random.GetF32Range(-1.0f, 1.0f);
+            }
+
+            if (posXZ.MagnitudeSquare() == 0.0f)
+                posXZ = (math::VEC3){ 0.0f, 0.0f, 0.0f };
+            else
+                posXZ.Normalize();
+
+            math::VEC3::Scale(&posXZ, &posXZ, data->_408);
+            math::VEC3::Add(&ptcl->velocity, &ptcl->velocity, &posXZ);
+        }
+
+        EmitCommon(emitter, ptcl);
+    }
+
+    emitter->isEmitted = true;
+    return ptclFirst;
+}
+
 } } // namespace nw::eft
